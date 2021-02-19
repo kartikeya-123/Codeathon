8730535edbdafc845147b54b035a7143e16bd42d
Kartikeya <56500864+kartikeya-123@users.noreply.github.com>
2021-02-19 15:10:57 +0530

    initialized new repo

---
+++ Server/routes/postRoutes.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const postController = require('./../controllers/postController');
+const authController = require('./../controllers/authController');
+const factory = require('./../controllers/handlerFactory');
+const router = express.Router();
+
+router
+  .route('/')
+  .get(postController.getAllPosts)
+  .post(authController.protect, postController.createPost);
+
+router.patch(
+  '/blacklist/:id',
+  authController.protect,
+  authController.restrictTo('admin'),
+  postController.BlacklistPost
+);
+router.patch('/upvote/:id', authController.protect, postController.upvotePost);
+router.patch(
+  '/downvote/:id',
+  authController.protect,
+  postController.downvotePost
+);
+router.patch(
+  '/edit/:id',
+  authController.protect,
+  postController.restrict,
+  postController.updatePost
+);
+router.get(
+  '/my-posts',
+  authController.protect,
+  postController.getUser,
+  postController.getAllPosts
+);
+router.route('/:id').get(postController.checkBlacklist, postController.getPost);
+
+router.delete(
+  '/delete/:id',
+  authController.protect,
+  // authController.restrictTo('admin'),
+  postController.restrict,
+  postController.deletePost
+);
+
+module.exports = router;
