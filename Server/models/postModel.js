badbf018e3d235ad2f8fda4068d6e3d0d029d83b
Mintu Jupally <mintu.jupally@gmail.com>
2021-02-20 12:21:29 +0530

    Modified buttons

--- Server/models/postModel.js
+++ Server/models/postModel.js
@@ -36,7 +36,7 @@ const postSchema = new mongoose.Schema(
     },
     createdAt: {
       type: Date,
-      default: Date.now(),
+      default: Date.now,
     },
     Blacklist: {
       type: Boolean,
