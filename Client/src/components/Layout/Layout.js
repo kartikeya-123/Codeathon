2ee459b8f1981f10a3bfb4abdb9496996776413b
Mintu Jupally <mintu.jupally@gmail.com>
2021-02-20 22:36:26 +0530

    Forgot password

--- Client/src/components/Layout/Layout.js
+++ Client/src/components/Layout/Layout.js
@@ -1,33 +1,33 @@
-import React, { Component } from "react";
-import Aux from "./../../hoc/Auxil/Auxil";
-import classes from "./Layout.css";
-import Toolbar from "./../UI/Toolbar/Toolbar";
-import Posts from "./../../containers/Posts/Posts";
-import FullPost from "./../../containers/Posts/FullPost";
-import NewPost from "./../../containers/NewPost/NewPost";
-import Login from "../../containers/Auth/Login/Login";
-import Signup from "../../containers/Auth/SignUp/Signup";
-import ForgotPassword from "../../containers/Auth/ForgotPassword";
-import ChangePassword from "../../containers/Auth/ChangePassword";
-import VerifyEmail from "../../containers/Auth/VerifyEmail";
-import { Route, Switch, withRouter, Redirect } from "react-router-dom";
-import User from "./../Profile/User/User";
-import MyPosts from "./../Profile/MyPosts/MyPosts";
-import EditPost from "./../Profile/MyPosts/EditPost";
-import axios from "axios";
-import UserContext, { UserProvider } from "./../../hoc/Context/UserContext";
-import Sidedrawer from "./../Navigation/SideDrawer/SideDrawer";
+import React, { Component } from 'react';
+import Aux from './../../hoc/Auxil/Auxil';
+import classes from './Layout.css';
+import Toolbar from './../UI/Toolbar/Toolbar';
+import Posts from './../../containers/Posts/Posts';
+import FullPost from './../../containers/Posts/FullPost';
+import NewPost from './../../containers/NewPost/NewPost';
+import Login from '../../containers/Auth/Login/Login';
+import Signup from '../../containers/Auth/SignUp/Signup';
+import ForgotPassword from '../../containers/Auth/ForgotPassword';
+import ChangePassword from '../../containers/Auth/ChangePassword';
+import VerifyEmail from '../../containers/Auth/VerifyEmail';
+import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
+import User from './../Profile/User/User';
+import MyPosts from './../Profile/MyPosts/MyPosts';
+import EditPost from './../Profile/MyPosts/EditPost';
+import axios from 'axios';
+import UserContext, { UserProvider } from './../../hoc/Context/UserContext';
+import Sidedrawer from './../Navigation/SideDrawer/SideDrawer';
 class Layout extends Component {
   state = {
     isLoggedin: false,
-    userRole: "",
+    userRole: '',
     user: [],
     showSideDrawer: false,
   };
   static contextType = UserContext;
   checkIsLoggedIn = () => {
     axios
-      .get("/api/v1/users/loginStatus", {
+      .get('/api/v1/users/loginStatus', {
         withCredentials: true,
       })
       .then((response) => {
@@ -44,14 +44,14 @@ class Layout extends Component {
   };
   logoutUserHandler = (props) => {
     axios
-      .get("/api/v1/users/logout", {
+      .get('/api/v1/users/logout', {
         withCredentials: true,
       })
       .then((response) => {
         // console.log(response.data);
         // this.setState({ isLoggedin: false });
 
-        this.props.history.push("/");
+        this.props.history.push('/');
         window.location.reload();
       })
       .catch((err) => console.log(err));
@@ -96,6 +96,11 @@ class Layout extends Component {
               <Switch>
                 <Route path="/signup" exact component={Signup} />
                 <Route path="/" exact component={Login} />
+                <Route
+                  path="/forgotPassword"
+                  exact
+                  component={ForgotPassword}
+                />
               </Switch>
             ) : (
               <Switch>
@@ -104,11 +109,6 @@ class Layout extends Component {
                 <Route path="/my-posts" exact component={MyPosts} />
                 <Route path="/edit/:id" exact component={EditPost} />
                 <Route path="/post/:id" exact component={FullPost} />
-                <Route
-                  path="/forgotPassword"
-                  exact
-                  component={ForgotPassword}
-                />
                 <Route
                   path="/changePassword"
                   exact
